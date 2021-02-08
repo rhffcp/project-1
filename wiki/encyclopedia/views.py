dd97b1dea72264c8d7669c9c71f19aec0d702130
Yeram Yoon <yoon@Yerams-MBP.attlocal.net>
2021-02-07 19:49:27 -0600

    added search function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -19,3 +18,24 @@ def entry(request, entry):
             "entry_title": entry,
             "contents": entry_page
         })
+
+def search(request):
+    search_value = request.GET.get('q','')
+    entry_page = util.get_entry(search_value)
+    entries = util.list_entries()
+
+    suggestions = []
+    for entry in entries:
+        if search_value.upper() in entry.upper():
+            suggestions.append(entry)
+
+    if search_value in entries:
+        return render(request, "encyclopedia/entry.html", {
+            "entry_title": search_value,
+            "contents": entry_page
+    })
+    else:
+        return render(request, "encyclopedia/suggestions.html", {
+            "suggestions": suggestions
+        })
+
